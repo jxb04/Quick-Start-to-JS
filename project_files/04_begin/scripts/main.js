bb5e6fdb7e1d0da9019e13a9af3836ada0ed33e5
Joel Bernasky <joel_bernasky@hotmail.com>
2017-11-13 09:38:21 -0500

    begin 04

--- project_files/04_begin/scripts/main.js
+++ project_files/04_begin/scripts/main.js
@@ -6,3 +6,45 @@
  * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
  * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
  */
+
+2 + 2;
+/*
+4
+*/
+
+2 - 2;
+/*
+0
+*/
+323246548654321 + 23465465435321;
+/*
+346712014089642
+*/
+
+2 * 2;
+
+
+2 / 2;
+
+/*
+1
+*/
+
+10 % 3;
+
+/*
+1
+*/
+
+30 - 2 * 3;
+
+/*
+24
+*/
+
+(30 - 2) * 3;
+
+/*
+84
+*/
+
