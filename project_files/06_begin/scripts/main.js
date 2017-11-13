d61084d58c6a07a8045bf7f465363f6c7b2d4528
Joel Bernasky <joel_bernasky@hotmail.com>
2017-11-13 10:48:34 -0500

    begin 06

---
+++ project_files/06_begin/scripts/main.js
@@ -0,0 +1,74 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+console.log("asl;dfj09834");
+2 + 2;
+
+/*
+4
+*/
+2 + "2";
+/*
+22
+*/
+
+"Hello " + "World!";
+/*
+Hello World!
+*/
+
+10 > 2;
+/*
+true
+*/
+
+10 < 2;
+/*
+false
+*/
+
+
+typeof(10 < 2);
+/*
+boolean
+*/
+
+typeof(300 / 2);
+
+/*
+number
+*/
+typeof("Hello " + "World");
+/*
+string
+*/
+
+typeof(4 < 2);
+
+/*
+boolean
+*/
+
+a + b;
+
+/*
+Exception: ReferenceError: a is not defined
+@Scratchpad/5:58:1
+*/
+typeof(a + b);
+/*
+Exception: ReferenceError: a is not defined
+@Scratchpad/5:58:1
+*/
+
+typeof(3 + "6");
+/*
+Exception: ReferenceError: a is not defined
+@Scratchpad/5:58:1
+*/
