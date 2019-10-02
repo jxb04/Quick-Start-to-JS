6f766fa8f9926590d01c254a1a8d920f200c4f4b
Joel Bernasky <joel_bernasky@hotmail.com>
2019-10-02 16:20:17 -0400

    object methods

--- project_files/Quick Start to JavaScript_Volume 3/03_begin/scripts/main.js
+++ project_files/Quick Start to JavaScript_Volume 3/03_begin/scripts/main.js
@@ -4,4 +4,16 @@ var myCoffee = {
   ounces: 100,
   milk: true,
   
+  reheat: function() {
+    if (myCoffee.temperature === "cold") {
+      myCoffee.temperature = "piping hot";
+      alert("your coffee has been reheated");
+    }
+  }
+  
 };
+
+myCoffee.temperature = "cold";
+myCoffee["temperature"] = "lukewarm";
+
+myCoffee.reheat();
