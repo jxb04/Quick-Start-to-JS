9c32fd14bd9f9608a7c5c7cd81216c92e0b79194
Joel Bernasky <joel_bernasky@hotmail.com>
2019-10-02 16:00:41 -0400

    creating objects

---
+++ project_files/Quick Start to JavaScript_Volume 3/02_begin/scripts/main.js
@@ -0,0 +1,13 @@
+var coffeeFlavor = "espresso";
+var coffeeTemperature = "piping hot";
+var coffeeOunces = 100;
+var coffeeMilk = false;
+
+var myCoffee = {
+    flavor: "espresso",
+    temperature: "piping hot",
+    ounces: "100",
+    milk: false
+};
+
+alert(myCoffee.ounces);
