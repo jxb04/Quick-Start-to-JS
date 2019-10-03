3e3d524c73be2c7118fa16ebb837e6d1f8b9446c
Joel Bernasky <joel_bernasky@hotmail.com>
2019-10-03 12:13:04 -0400

    fix linting errors

--- project_files/Quick Start to JavaScript_Volume 3/09_begin/scripts/main.js
+++ project_files/Quick Start to JavaScript_Volume 3/09_begin/scripts/main.js
@@ -1,15 +1,16 @@
-alert("Hello World!");
+window.alert("Hello World!");
 
-function lameJoke(){ 
-  randomNumber = Math.round(Math.random() * 2);
-   if(randomNumber === 1){
-    alert("How many tickles does it take to make an octopus laugh? Ten tickles.");
+function lameJoke() {
+  "use strict";
+  var randomNumber = Math.round(Math.random() * 2);
+  if (randomNumber === 1) {
+    window.alert("How many tickles does it take to make an octopus laugh? Ten tickles.");
   } else if (randomNumber === 2) {
-     alert("Why did the scarecrow win an award? Because he was outstanding in his field.");
+    window.alert("Why did the scarecrow win an award? Because he was outstanding in his field.");
   } else {
-    alert("Why did the chicken cross the road? To get to the other side!");
-    
+    window.alert("Why did the chicken cross the road? To get to the other side!");
+
   }
 }
 
-lameJoke();  
+lameJoke();
